e142a8e1c625ab17a62c0649d426061271c8a4f4
Mont0360 <mont0360@umn.edu>
2020-06-30 16:12:40 -0500

    added seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Joe Mauer Burger');
+INSERT INTO burgers (burger_name) VALUES ('Sweet Burger');
+INSERT INTO burgers (burger_name) VALUES ('Juicy Lucy');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Little Mac');
